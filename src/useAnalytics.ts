c2c35f5af12229b0d67d6ecb5018ff50902e9aac
Epicka Owca <epickaowca@Epickas-Laptop.local>
2024-06-07 23:09:17 +0200

    add extra param

--- src/useAnalytics.ts
+++ src/useAnalytics.ts
@@ -1,17 +1,24 @@
 import { useEffect } from "react";
 
-export const useAnalytics = () => {
+export const useAnalytics = (
+  projectName:
+    | "audiophile-ecommerce-website"
+    | "google-drive-clone"
+    | "interactive-comments-section"
+    | "multi-step-form"
+) => {
   useEffect(() => {
     const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    fetch(
-      "https://web-analytics-sigma.vercel.app/audiophile-ecommerce-website/api",
-      {
+    try {
+      fetch("https://web-analytics-sigma.vercel.app/api", {
         method: "POST",
-        body: JSON.stringify({ userTimeZone }),
+        body: JSON.stringify({ userTimeZone, projectName }),
         headers: {
           "Content-Type": "application/json",
         },
-      }
-    );
+      });
+    } catch (err) {
+      console.log(err);
+    }
   }, []);
 };
