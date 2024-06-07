82504b341e634607913cf23bf841573ebe751ff6
Epicka Owca <epickaowca@Epickas-Laptop.local>
2024-06-07 18:44:36 +0200

    remove utils, fetch with userTimeZone

--- src/useAnalytics.ts
+++ src/useAnalytics.ts
@@ -1,10 +1,17 @@
 import { useEffect } from "react";
-import { getBrowser } from "./utils";
 
 export const useAnalytics = () => {
   useEffect(() => {
     const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    console.log(userTimeZone);
-    console.log(getBrowser());
+    fetch(
+      "https://web-analytics-sigma.vercel.app/audiophile-ecommerce-website/api",
+      {
+        method: "POST",
+        body: JSON.stringify({ userTimeZone }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
   }, []);
 };
