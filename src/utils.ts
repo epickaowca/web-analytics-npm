5a128128a9f7e5fd70220a949a4ea305a99cd1db
Epicka Owca <epickaowca@Epickas-Laptop.local>
2024-06-07 16:53:21 +0200

    return browser without version

--- src/utils.ts
+++ src/utils.ts
@@ -28,57 +28,5 @@ export const getBrowser = () => {
       : browser;
   browser = /opr|opera/i.test(userAgent) ? "Opera" : browser;
 
-  // detect browser version
-  switch (browser) {
-    case "UCBrowser":
-      return `${browser}/${browserVersion(
-        userAgent,
-        /(ucbrowser)\/([\d\.]+)/i
-      )}`;
-    case "Edge":
-      return `${browser}/${browserVersion(
-        userAgent,
-        /(edge|edga|edgios|edg)\/([\d\.]+)/i
-      )}`;
-    case "GoogleBot":
-      return `${browser}/${browserVersion(
-        userAgent,
-        /(googlebot)\/([\d\.]+)/i
-      )}`;
-    case "Chromium":
-      return `${browser}/${browserVersion(
-        userAgent,
-        /(chromium)\/([\d\.]+)/i
-      )}`;
-    case "Firefox":
-      return `${browser}/${browserVersion(
-        userAgent,
-        /(firefox|fxios)\/([\d\.]+)/i
-      )}`;
-    case "Chrome":
-      return `${browser}/${browserVersion(
-        userAgent,
-        /(chrome|crios)\/([\d\.]+)/i
-      )}`;
-    case "Safari":
-      return `${browser}/${browserVersion(userAgent, /(safari)\/([\d\.]+)/i)}`;
-    case "Opera":
-      return `${browser}/${browserVersion(
-        userAgent,
-        /(opera|opr)\/([\d\.]+)/i
-      )}`;
-    case "IE":
-      const version = browserVersion(userAgent, /(trident)\/([\d\.]+)/i);
-      // IE version is mapped using trident version
-      // IE/8.0 = Trident/4.0, IE/9.0 = Trident/5.0
-      return version
-        ? `${browser}/${parseFloat(version) + 4.0}`
-        : `${browser}/7.0`;
-    default:
-      return `unknown/0.0.0.0`;
-  }
-};
-
-const browserVersion = (userAgent: any, regex: any) => {
-  return userAgent.match(regex) ? userAgent.match(regex)[2] : null;
+  return browser;
 };
