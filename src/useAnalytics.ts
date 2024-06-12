8c7713cd2809c04b2bdedad78df3f498f274c9d7
Epicka Owca <epickaowca@Epickas-Laptop.local>
2024-06-12 23:09:46 +0200

    send analytics every hour

--- src/useAnalytics.ts
+++ src/useAnalytics.ts
@@ -8,7 +8,18 @@ export const useAnalytics = (
     | "multi-step-form"
 ) => {
   useEffect(() => {
+    const timeStamp = localStorage.getItem("OO_WB_TimeStamp");
+    if (timeStamp) {
+      const now = new Date();
+      const isOneHourPass =
+        now.getTime() - new Date(timeStamp).getTime() >= 3600000;
+      if (!isOneHourPass) {
+        return () => {};
+      }
+    }
+
     const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    localStorage.setItem("OO_WB_TimeStamp", new Date().toString());
     try {
       fetch("https://web-analytics-sigma.vercel.app/api", {
         method: "POST",
