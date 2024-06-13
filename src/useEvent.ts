1eb0b9fe0d2032a336b1513b1374d969c84bb265
Epicka Owca <epickaowca@Epickas-Laptop.local>
2024-06-13 11:24:50 +0200

    fix useEvent args

--- src/useEvent.ts
+++ src/useEvent.ts
@@ -1,12 +1,12 @@
 import { BASE_URL, AVAILABLE_PROJECTS, currentProject } from "./constants";
 
-export const useEvent = (eventName: string, label: string) => {
+export const useEvent = () => {
   //@ts-ignore
   const projectName = AVAILABLE_PROJECTS[currentProject];
   if (!projectName)
     throw new Error("incorrect variable REACT_APP_WA_PROJECT_NAME");
 
-  return () => {
+  return (eventName: string, label: string) => {
     try {
       fetch(`${BASE_URL}/event/api`, {
         method: "POST",
