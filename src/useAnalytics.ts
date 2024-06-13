f9d4aafea624a40c435932701f8d6b154970d9e7
Epicka Owca <epickaowca@Epickas-Laptop.local>
2024-06-13 10:59:40 +0200

    fix constants project

--- src/useAnalytics.ts
+++ src/useAnalytics.ts
@@ -5,9 +5,6 @@ export const useAnalytics = () => {
   useEffect(() => {
     //@ts-ignore
     const projectName = AVAILABLE_PROJECTS[currentProject];
-    console.log("AVAILABLE_PROJECTS", AVAILABLE_PROJECTS);
-    console.log("currentProject", currentProject);
-    console.log("projectName", projectName);
 
     if (!projectName) {
       throw new Error("incorrect variable REACT_APP_WA_PROJECT_NAME");
